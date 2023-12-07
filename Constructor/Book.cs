f79e375d3f1893325132dca0254dbadcf2c85d9f
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-07 11:09:55 +0200

    Add project files.

---
+++ Constructor/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, String aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry", "JK rowling", 600);
+
+
+            Book book2 = new Book("Lord of the rings","Tolkein",900);
+            
+            Console.WriteLine(book1.title);
+            Console.ReadKey();
+        }
+    }
+}
